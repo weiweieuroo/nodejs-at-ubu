ce6cd9ae4101dda64f3edc0e3764b8be9a30cf6d
beyondkk <Kitipongbigbank@gmail.com>
2017-09-07 08:48:51 +0700

    add kitipong

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2>');
+    res.end('<h1>Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2>
+            <hr><h1>Hi</h1><h2>5711403148 Kitipong  Sudcha</h2>');
 }).listen(8080);
