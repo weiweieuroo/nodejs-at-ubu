213cd480f0bcbcccb0ef3d45aeaa115529f5c6ab
beyondkk <kitipong.su.57@ubu.ac.th>
2017-09-07 10:05:53 +0700

    change color

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1 style="color : red;">Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2><hr><h1>Hi</h1><h2>5711403148 Kitipong  Sudcha</h2>');
+    res.end('<h1 style="color : red;">Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2><hr><h1 style="color : blue;">Hi</h1><h2 style="color : yellow; background-color:dddddd;">5711403148 Kitipong  Sudcha</h2>');
 }).listen(8080);
