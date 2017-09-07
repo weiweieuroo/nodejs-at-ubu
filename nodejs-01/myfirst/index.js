84819c2f5b903283996be6b4130c9b0b6fe7475a
weiweieuroo <korawit.in.57@ubu.ac.th>
2017-09-07 09:39:22 +0000

    ..

--- nodejs-01/myfirst/index.js
+++ nodejs-01/myfirst/index.js
@@ -2,6 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2>
-            <hr><h1>Hi</h1><h2>5711403148 Kitipong  Sudcha</h2>');
+    res.end('<h1 style="color : red;">Hello World!</h1><h2>5711407056</h2><h2>กรวิชญ์ อินทรวัลณ์กูล</h2><hr><h1>Hi</h1><h2>5711403148 Kitipong  Sudcha</h2>');
 }).listen(8080);
